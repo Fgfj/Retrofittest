47d3eb58330f63e4b0590228c78528c7f5bcb4e2
yuzhou <975906711@qq.com>
2016-11-04 10:43:16 +0800

    test

--- app/src/main/java/testvh/yz/com/RongyunIM/Rongyun_Act.java
+++ app/src/main/java/testvh/yz/com/RongyunIM/Rongyun_Act.java
@@ -1,5 +1,6 @@
 package testvh.yz.com.RongyunIM;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.widget.TextView;
 
 import io.rong.imkit.RongIM;
 import io.rong.imlib.model.Conversation;
+import io.rong.imlib.model.UserInfo;
 import testvh.yz.com.retrofittest.R;
 
 /**
@@ -23,15 +25,16 @@ public class Rongyun_Act extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.rongyunserverconntect);
         RongyunServer.connect(this, "IVWXLYxksSQoOJknQ0q9TYmXK11Lcjn0DpCqMMGvdhcIglhjRbeHebH/J4CRIhI5xVsSnH4vVdcAQtrbKVn1aw==",
                 new RongyunServer.Dopost() {
-            @Override
-            public void poststirng(String s) {
-                textView.setText(s);
-            }
-            @Override
-            public void errorpoststirng(String s) {
-                textView.setText(s);
-            }
-        });
+                    @Override
+                    public void poststirng(String s) {
+                        textView.setText(s);
+                    }
+
+                    @Override
+                    public void errorpoststirng(String s) {
+                        textView.setText(s);
+                    }
+                });
     }
     public void 启动会话界面(View view) {
         //启动会话界面
@@ -50,4 +53,27 @@ public class Rongyun_Act extends AppCompatActivity {
         if (RongIM.getInstance() != null)
             RongIM.getInstance().startSubConversationList(this, Conversation.ConversationType.GROUP);
     }
+
+    public void 设置用户信息(View view) {
+        RongIM.setUserInfoProvider(new RongIM.UserInfoProvider() {
+            @Override
+            public UserInfo getUserInfo(String s) {
+                return new UserInfo("1", "设置以后的名字", Uri.parse("https://gss0.bdstatic.com/70cFsj3f_gcX8t7mm9GUKT-xh_/avatar/100/r6s1g1.gif"));
+            }
+        },true);
+    }
+
+
+    public void 刷新用户信息(View view) {
+        /**
+         * 刷新用户缓存数据。
+         *
+         * @param userInfo 需要更新的用户缓存数据。
+         */
+        RongIM.getInstance().refreshUserInfoCache(
+                new UserInfo("1", "刷新以后的名字", Uri.parse("http://rongcloud-web.qiniudn.com/docs_demo_rongcloud_logo.png")));
+
+    }
+
+
 }
