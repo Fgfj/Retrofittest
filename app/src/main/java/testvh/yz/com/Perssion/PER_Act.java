4bf9c02679fe7a444fbf0cfb332f65521c8e48ca
yuzhou <975906711@qq.com>
2016-11-08 10:37:50 +0800

    qqq

---
+++ app/src/main/java/testvh/yz/com/Perssion/PER_Act.java
@@ -0,0 +1,15 @@
+package testvh.yz.com.Perssion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import testvh.yz.com.retrofittest.R;
+
+public class PER_Act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_per_);
+    }
+}
