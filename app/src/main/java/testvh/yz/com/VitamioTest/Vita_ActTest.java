c1a696e5701a2f3a0fe718523b9a1bc662b9e613
yuzhou <975906711@qq.com>
2016-11-14 13:21:04 +0800

    vitamio

---
+++ app/src/main/java/testvh/yz/com/VitamioTest/Vita_ActTest.java
@@ -0,0 +1,15 @@
+package testvh.yz.com.VitamioTest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import testvh.yz.com.retrofittest.R;
+
+public class Vita_ActTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vita__act_test);
+    }
+}
