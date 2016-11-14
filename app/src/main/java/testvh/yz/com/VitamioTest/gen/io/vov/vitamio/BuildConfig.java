b77cb658b996ca48b067a66799a991704803f167
yuzhou <975906711@qq.com>
2016-11-14 15:28:56 +0800

    vitamio依赖包

---
+++ app/src/main/java/testvh/yz/com/VitamioTest/gen/io/vov/vitamio/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package testvh.yz.com.VitamioTest.gen.io.vov.vitamio;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
